294af796a1451d79a88805310f1e896e9a91f026
DuhaKahya <duhakahyaa@gmail.com>
2023-09-18 12:15:15 +0200

    1e versie
    Lege bestand als test

---
+++ DAL/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Your code goes here
+        }
+    }
+}
---
+++ UI/NoSQL-Project.cs
@@ -0,0 +1,31 @@
+
+using Model;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Xml.Serialization;
+using static System.Windows.Forms.LinkLabel;
+using Timer = System.Windows.Forms.Timer;
+
+namespace UI
+{
+    public partial class NoSQL : Form
+    {
+
+
+        public NoSQL()
+        {
+
+            InitializeComponent();
+
+
+
+        }
+
+        private void NoSQL_Load(object sender, EventArgs e)
+        {
+            //TESTETSTETSTETSTE
+            Console.WriteLine();
+        }
+    }
+}
+
